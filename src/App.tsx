589a6836d673a0cbdff0b238be05be780d5cf990
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-29 15:24:32 +0700

    add more customized button components

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,51 @@ const App = () => {
 				>
 					<Button>Default</Button>
 					<Button disabled>Disable</Button>
+					<Button
+						colors={{
+							bg: "bg-amber-500",
+							hoverBg: "hover:bg-amber-600",
+							focusRing: "focus:ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-rose-500",
+							hoverBg: "hover:bg-rose-600",
+							focusRing: "focus:ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-fuchsia-500",
+							hoverBg: "hover:bg-fuchsia-600",
+							focusRing: "focus:ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-indigo-500",
+							hoverBg: "hover:bg-indigo-600",
+							focusRing: "focus:ring-indigo-500",
+						}}
+					>
+						Indigo
+					</Button>
+					<Button
+						colors={{
+							bg: "bg-blue-500",
+							hoverBg: "hover:bg-blue-600",
+							focusRing: "focus:ring-blue-500",
+						}}
+					>
+						Blue
+					</Button>
 				</Section>
 
 				<Section
@@ -26,6 +71,61 @@ const App = () => {
 					<Button variant="secondary">Default</Button>
 					<Button disabled variant="secondary">
 						Disable
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-amber-500",
+							hoverBg: "hover:bg-amber-500",
+							hoverBorder: "border-amber-500",
+							focusRing: "ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-rose-500",
+							hoverBg: "hover:bg-rose-500",
+							hoverBorder: "border-rose-500",
+							focusRing: "ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-fuchsia-500",
+							hoverBg: "hover:bg-fuchsia-500",
+							hoverBorder: "border-fuchsia-500",
+							focusRing: "ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-indigo-500",
+							hoverBg: "hover:bg-indigo-500",
+							hoverBorder: "border-indigo-500",
+							focusRing: "ring-indigo-500",
+						}}
+					>
+						Indigo
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-blue-500",
+							hoverBg: "hover:bg-blue-500",
+							hoverBorder: "border-blue-500",
+							focusRing: "ring-blue-500",
+						}}
+					>
+						Blue
 					</Button>
 				</Section>
 
@@ -37,14 +137,92 @@ const App = () => {
 					<Button disabled variant="tertiary">
 						Disable
 					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-amber-500",
+							hoverText: "hover:text-amber-700",
+							focusRing: "focus:ring-amber-500",
+						}}
+					>
+						Amber
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-rose-500",
+							hoverText: "hover:text-rose-700",
+							focusRing: "focus:ring-rose-500",
+						}}
+					>
+						Rose
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-fuchsia-500",
+							hoverText: "hover:text-fuchsia-700",
+							focusRing: "focus:ring-fuchsia-500",
+						}}
+					>
+						Fuchsia
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-indigo-500",
+							hoverText: "hover:text-indigo-700",
+							focusRing: "focus:ring-indigo-500",
+						}}
+					>
+						Indigo
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-blue-500",
+							hoverText: "hover:text-blue-700",
+							focusRing: "focus:ring-blue-500",
+						}}
+					>
+						Blue
+					</Button>
 				</Section>
 
 				<Section
 					title="Destructive"
 					description="Destructive actions typically result in the removal of an item, and may result in an undesirable consequence."
 				>
-					<div className="bg-blue-500 p-3">component 1</div>
-					<div className="bg-blue-500 p-3">component 2</div>
+					<Button
+						colors={{
+							bg: "bg-red-500",
+							hoverBg: "hover:bg-red-600",
+							focusRing: "focus:ring-red-500",
+						}}
+					>
+						Yes, I'm sure I want to delete this
+					</Button>
+					<Button
+						variant="secondary"
+						colors={{
+							text: "text-red-500",
+							hoverBg: "hover:bg-red-600",
+							hoverBorder: "hover:border-red-500",
+							focusRing: "focus:ring-red-500",
+						}}
+					>
+						Unsubscribe
+					</Button>
+					<Button
+						variant="tertiary"
+						colors={{
+							text: "text-red-500",
+							hoverText: "hover:text-red-600",
+							focusRing: "focus:ring-red-500",
+						}}
+					>
+						Cancel
+					</Button>
 				</Section>
 			</Container>
 
