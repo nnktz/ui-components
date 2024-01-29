32acba07ebe2398b8b112602e69d83c0ff6654ea
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-29 16:46:45 +0700

    create useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,24 @@
+import React from "react"
+
+export const useTheme = (storageKey = "vite-ui-theme") => {
+	const [isDarkMode, setIsDarkMode] = React.useState<boolean>(() => {
+		return localStorage.getItem(storageKey)
+			? localStorage.getItem(storageKey) === "true"
+			: window.matchMedia("(prefers-color-scheme: dark)").matches
+	})
+
+	React.useEffect(() => {
+		const root = window.document.documentElement
+
+		root.classList.remove("light", "dark")
+		root.classList.add(isDarkMode ? "dark" : "light")
+		localStorage.setItem(storageKey, isDarkMode.toString())
+	}, [isDarkMode, storageKey])
+
+	const toggleDarkMode = () => setIsDarkMode(!isDarkMode)
+
+	return {
+		isDarkMode,
+		toggleDarkMode,
+	}
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import formsPlugin from "@tailwindcss/forms"
 
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: ["class"],
 	theme: {
 		extend: {},
 	},
