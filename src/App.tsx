87498d9c4dcbf84936da23eca6132abf848f07ac
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-29 13:51:07 +0700

    main content area

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,75 @@ const App = () => {
 			</header>
 
 			{/* main content are */}
+
+			<main className="p-10 sm:px-16 lg:px-44">
+				<div className="mx-auto max-w-3xl space-y-20">
+					{/* content container */}
+					<header>
+						<h1 className="block text-2xl font-bold text-gray-800 sm:text-3xl">
+							Component name
+						</h1>
+
+						<p className="mt-2 text-lg text-gray-800">Component description</p>
+					</header>
+
+					<div className="space-y-10 md:space-y-16">
+						{/* content section */}
+
+						<div>
+							{/* section heading */}
+							<h2 className="text-lg font-bold text-gray-800">
+								Section heading
+							</h2>
+
+							{/* section description */}
+							<p className="mt-1 text-gray-600">Section description text</p>
+
+							{/* section preview area */}
+							<div className="mt-3">
+								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
+									<div className="flex flex-wrap gap-2">
+										<div className="bg-blue-500 p-3">component 1</div>
+										<div className="bg-blue-500 p-3">component 2</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<header>
+						<h1 className="block text-2xl font-bold text-gray-800 sm:text-3xl">
+							Component name
+						</h1>
+
+						<p className="mt-2 text-lg text-gray-800">Component description</p>
+					</header>
+
+					<div className="space-y-10 md:space-y-16">
+						{/* content section */}
+
+						<div>
+							{/* section heading */}
+							<h2 className="text-lg font-bold text-gray-800">
+								Section heading
+							</h2>
+
+							{/* section description */}
+							<p className="mt-1 text-gray-600">Section description text</p>
+
+							{/* section preview area */}
+							<div className="mt-3">
+								<div className="flex flex-col rounded-xl border p-6 shadow-sm">
+									<div className="flex flex-wrap gap-2">
+										<div className="bg-blue-500 p-3">component 1</div>
+										<div className="bg-blue-500 p-3">component 2</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
 		</div>
 	)
 }
